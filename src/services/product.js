26c8aa7e098c9943b92ea89852f7b30fdb229f1e
carlosziegler <carlos.ziegler@gmail.com>
2021-03-21 15:49:20 +0100

    add axios api config

--- src/services/product.js
+++ src/services/product.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axiosApi from '../api/axiosApi';
 
 export async function getAllProducts() {
-  const result = await axios.get('http://localhost:3000/product');
+  const result = await axiosApi.get('/product');
   return result.data;
 }
